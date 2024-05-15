975875190af488c2c07384adee36e31597458850
lizzbb <158301965+lizzbb@users.noreply.github.com>
2024-05-15 08:41:55 -0600

    Laboratorio 7
    Carpeta C#

---
+++ L7 - Lizbeth Herrera - 1246024/Program.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.VisualBasic;
+
+class Program 
+{
+    static void Main(string[] args)
+    {
+
+        Console.WriteLine("Laboratorio No. 07 - Lizbeth Herrera - 1246024"); 
+        Console.WriteLine();
+
+        int NFibonacci; 
+        bool conver = false;
+        bool nPositivo = false;
+
+        do
+        {
+            Console.WriteLine("Ingrese un número mayor a 0");
+            conver = int.TryParse(Console.ReadLine(), out NFibonacci);
+            Console.WriteLine(); 
+
+            if(conver)
+            {
+                if(NFibonacci > 0)
+                {
+                    nPositivo = true;
+                }
+            }
+        }
+        while(!conver || !nPositivo);
+
+        string resultado = ""; 
+        int A = 0;
+        int B = 1;
+        int C = 0;
+        int i = 2;
+
+        if(NFibonacci > 0)
+        {
+            resultado = A + ", ";
+
+            if(NFibonacci > 1)
+            {
+                resultado += B + ", ";
+
+                do
+                {
+                    C = A + B;
+                    resultado += C + ", ";
+                    A = B;
+                    B = C;
+                    i = i + 1;
+
+                }
+                while(i < NFibonacci);
+            
+            Console.WriteLine(resultado);   
+            }
+        }
+    
+    }
+}
