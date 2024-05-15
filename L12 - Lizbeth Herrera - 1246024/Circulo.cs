8cc3d49dabda36f1be2de4af4773e8192d83231b
lizzbb <158301965+lizzbb@users.noreply.github.com>
2024-05-15 08:50:09 -0600

    Laboratorio 12

---
+++ L12 - Lizbeth Herrera - 1246024/Circulo.cs
@@ -0,0 +1,38 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace L12___Lizbeth_Herrera___1246024;
+
+public class Circulo
+{
+    private double radio; 
+     public Circulo(double radio)
+    {
+        this.radio = radio;
+        
+    }
+
+    private double ObtenerPerimetro(double radio)
+    {
+        return 2 * Math.PI * radio;
+    }
+
+    private double ObtenerArea(double radio)
+    {
+        return Math.PI * Math.Pow(2, radio);
+    }
+
+    private double ObtenerVolumen(double radio)
+    {
+        return (4 * Math.PI * Math.Pow(3, radio)) / 3;
+    }
+
+    public void CalcularGeometria(ref double unPerimetro, ref double unArea, ref double unVolumen)
+    {
+        unPerimetro = ObtenerPerimetro(radio);
+        unArea = ObtenerArea(radio);
+        unVolumen = ObtenerVolumen(radio);
+
+    }
+   
+
+}
---
+++ L12 - Lizbeth Herrera - 1246024/Program.cs
@@ -0,0 +1,23 @@
+﻿using L12___Lizbeth_Herrera___1246024;
+
+class Program
+{
+    public static void Main(string[] argrs)
+    {
+        Console.WriteLine("Ingrese el radio del círculo");
+        string Radio = Console.ReadLine(); 
+        double dRadio = double.Parse(Radio);
+        Console.WriteLine(); 
+        
+        Circulo objCirculo = new Circulo(dRadio);
+
+        double perimetro = 0;
+        double area = 0;
+        double volumen = 0; 
+
+        objCirculo.CalcularGeometria(ref perimetro, ref area, ref volumen);
+        Console.WriteLine("Perímetro: " + perimetro); 
+        Console.WriteLine("Área: " + area); 
+        Console.WriteLine("Volumen: " + volumen); 
+    }
+}
